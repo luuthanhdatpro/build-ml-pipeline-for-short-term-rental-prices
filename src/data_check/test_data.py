941ec634707b2c928cda0a2c3987276534ec12f9
Thanh Luu <congthanhluu@gmail.com>
2024-07-17 10:48:18 +0700

    Finishing ML pipeline

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -59,7 +59,14 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
+def test_row_count(data):
+    """
+    Test number of row with provided data set
+    """
+    assert 15000 < data.shape[0] < 1000000
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data, min_price, max_price):
+    """
+    Testing if price is within range
+    """
+    assert data['price'].between(min_price, max_price).all()
